71dadf90e6e3cc803d92304a06957116fedfc2a0
github username <github email address>
2020-01-16 18:40:40 -0800

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,41 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, only: [:show, :edit]
+
+    def index
+        @employees = Employee.all 
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to @employee 
+        else 
+            render :new 
+        end
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        
+        if @employee.save
+            redirect_to @employee
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url)
+    end
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
 end
